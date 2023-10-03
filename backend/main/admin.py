bf6caee4b449639989dfd9ae6c7a14add725c83c
appuser at t2 <level@gotovo.com>
2023-10-03 12:47:08 -0500

    Hide the Group admin section to reduce visual clutter
    It can be enabled for some user roles, or later at any time.
    Also: Register the models in the format that allows customization

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from .models import Forum, Opportunity, Tag, User, Waitlist
 
-admin.site.register(User)
-admin.site.register(Opportunity)
-admin.site.register(Forum)
-admin.site.register(Tag)
-admin.site.register(Waitlist)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Opportunity)
+class OpportunityAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Forum)
+class ForumAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Waitlist)
+class WaitlistAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.unregister(Group)
