efdfc3775df5ca96107ef16d4bcfa10cda75f36a
Leo-MathGuy <leo.mathguy@gmail.com>
2023-10-08 14:23:54 -0700

    added script imports and animation

---
+++ backend/main/templates/registration/registration.js
@@ -0,0 +1,44 @@
+let tl = gsap.timeline({
+    delay: 0.5,
+    defaults: {
+        duration: 1
+    }
+});
+
+let appHeight = document.getElementById("App").clientHeight;
+
+tl.to(
+    "#App", {
+    duration: 0,
+    y: -50,
+    height: 0
+}
+);
+
+tl.to(
+    "#App", {
+    y: 0,
+    opacity: 1,
+    height: appHeight
+}
+);
+
+tl.to(
+    "h2", {
+    duration: 0.3,
+    opacity: 1
+}, "<+0.4"
+);
+
+let formChild = document.getElementsByTagName("form")[0].children;
+
+for (let i = 0; i < formChild.length; i++) {
+    const cur_label = formChild[i];
+
+    tl.to(
+        cur_label, {
+        duration: 0.2,
+        opacity: 1
+    }, "<75%"
+    );
+}
