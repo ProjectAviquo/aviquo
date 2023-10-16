4e475b09e678c1ce53b4db54d005a4edbebed9db
Srivatsav21 <srivatsavkannan@gmail.com>
2023-10-16 08:37:50 +0530

    Landing: Centered it

--- backend/api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from rest_framework_api_key.admin import APIKey
-
-# Hide the "API Key Permissions" group in the admin to reduce clutter.
-# Consider: Enable the group in the future based on the admin user role
-admin.site.unregister(APIKey)
--- utils/compile_scss.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find . -type f -name '*.scss' | while read -r scss_file; do
-  css_file="${scss_file%.scss}.css"
-  echo "Compiling $scss_file to $css_file"
-  sass "$scss_file" "$css_file"
-done
