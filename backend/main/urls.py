4e1801cebeac1b11e445da0938c7544624352f13
Srivatsav21 <srivatsavkannan@gmail.com>
2023-10-29 10:58:13 +0530

    Can now follow and unfollow each other

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf import settings
 
 from .email_sender import CustomPasswordResetView
-from .views import ForumView, OpportunityView, SignUp, home, profile, clogout, waitlist, profilee, Login,  follow_opportunity, delete_forum
+from .views import ForumView, OpportunityView, SignUp, home, profile, clogout, waitlist, profilee, Login,  follow_opportunity, delete_forum, follow_user
 
 
 urlpatterns = [
@@ -17,6 +17,7 @@ urlpatterns = [
     path('profile/', profilee, name='profilee'),
     path('follow-opportunity/',  follow_opportunity, name=' follow_opportunity'),
     path('delete_forum/<int:forum_id>/', delete_forum, name='delete_forum'),
+    path('follow-user/', follow_user, name='follow_user'),
     path("password_reset/", CustomPasswordResetView.as_view(),
              name="password_reset"),
 ]
