aa63795bbed3e3666c1db80c9ffb3b3b9aae70fe
Srivatsav21 <srivatsavkannan@gmail.com>
2023-10-29 10:21:14 +0530

    Cleanup & Display posted forum

--- backend/main/forms.py
+++ backend/main/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
-from .models import Tag
-
+from .models import Tag, User, Waitlist, Forum
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class TagFilterForm(forms.Form):
     tags = forms.ModelMultipleChoiceField(
@@ -9,3 +9,33 @@ class TagFilterForm(forms.Form):
         widget=forms.CheckboxSelectMultiple,
         required=False,
     )
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "first_name", "last_name"]
+
+class AddWaitlistForm(forms.ModelForm):
+    class Meta:
+        model = Waitlist
+        fields = ["email"]
+
+class AddForumForm(forms.ModelForm):
+    class Meta:
+        model = Forum
+        fields = ["topic", "description"]
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)  # Make the email field required
+    class Meta:
+        model = User
+        fields = UserCreationForm.Meta.fields + ("email",)
+
+class CustomAuthForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomAuthForm, self).__init__(*args, **kwargs)
+        self.fields.pop('password1')
+        self.fields.pop('password2')
+
+    class Meta:
+        model = User
+        fields = ["username", "password"]
