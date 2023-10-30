31e9c36feadb688075f13e0f3b62840db900cf09
Leo-MathGuy <leo.mathguy@gmail.com>
2023-10-29 21:15:35 -0700

    Added empry links to navbar and updated my waitlist to match

--- backend/main/static/waitlist/waitlist.js
+++ backend/main/static/waitlist/waitlist.js
@@ -1,132 +1,122 @@
-$("document").ready(function () {
-    $("#boxes").scrollLeft($("#boxes").width() / 2);
+let tl = gsap.timeline({
+    defaults: {
+        duration: 0.2
+    }
+});
 
-    gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(EaselPlugin);
+gsap.registerPlugin(ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger);
 
-    $(window).scrollTop(0);
-
-    var tl = gsap.timeline({ defaults: { duration: 0.7 } });
-
-    tl.from("header h1", {
-        delay: 0.4,
-        opacity: 0,
-        scale: 0.95,
-        y: -20,
-        duration: 0.4
+function join_the_soviet_union() {
+    tl.clear();
+    tl.to("#wl-submit", {
+        filter: "hue-rotate(0deg)",
+        duration: 0
     });
-
-    tl.from("header h2", {
-        delay: 0.3,
-        opacity: 0,
-        scale: 0.9,
-        y: -20
-    });
-
-    tl.from(
-        "form",
-        {
-            opacity: 0,
-            scale: 0.9,
-            y: -20
-        },
-        "<30%"
-    );
-
-    tl.from(".arrow > *", {
-        y: -25,
-        opacity: 0,
+    tl.to(window, { duration: 0.5, scrollTo: "#App" });
+    tl.to("#wl-email", {
+        scale: 1.2,
         ease: "power2.out"
     });
+    tl.to("#wl-email", {
+        scale: 1,
+        ease: "power2.out"
+    });
+    tl.to("#wl-submit", {
+        scale: 1.2,
+        ease: "power2.out"
+    });
+    tl.to(
+        "#wl-submit",
+        {
+            filter: "hue-rotate(360deg)",
+            duration: 0.5
+        },
+        "<"
+    );
+    tl.to(
+        "#wl-submit",
+        {
+            scale: 1,
+            ease: "power2.out"
+        },
+        "<0.15"
+    );
+}
+var fadeTl = gsap.timeline({ defaults: { duration: 1, ease: "power2.out" } });
 
-    tl.to(".arrow", {
+fadeTl.from("h1", { opacity: 0, delay: 0.3, y: -20 });
+fadeTl.from("header h3", { opacity: 0, y: -20 }, "<30%");
+fadeTl.from("form", { opacity: 0, y: -20 }, "<30%");
+fadeTl.from("#sponsors", { opacity: 0, y: -20, duration: 0.7 }, "<30%");
+fadeTl.from("#sponsors aside", { x: "-100vw" }, "<-0.4");
+
+var quotelines = document.querySelectorAll("#quote-text > p > *");
+function fade_scroll(selector) {
+    fadeTl.from(selector, {
         opacity: 0,
-        y: 25,
+        y: "-40px",
         scrollTrigger: {
-            trigger: "header",
-            start: "90.9% bottom",
+            trigger: selector,
+            start: "bottom bottom",
             end: "+=100",
-            scrub: true
+            scrub: 1,
+            once: true
         }
     });
+}
+for (let i = 0; i < quotelines.length; i++) {
+    const element = quotelines[i];
+    if (element.tagName == "BR") {
+        continue;
+    }
+    fade_scroll(element)
+}
 
-    gsap.fromTo(
-        "#box-1",
-        {
-            scale: 0.75
-        },
-        {
-            scale: 0.9,
-            scrollTrigger: {
-                scroller: "#boxes",
-                trigger: "#boxes",
-                start: "0% left",
-                end: "+=60%",
-                horizontal: true,
-                scrub: true,
-                snap: {
-                    snapTo: 1,
-                    duration: { min: 0.2, max: 0.5 },
-                    delay: 0
-                }
-            }
-        }
-    );
+fade_scroll("#introducing h5");
+fade_scroll("#introducing h2");
 
-    gsap.fromTo(
-        "#box-1",
-        {
-            scale: 0.9
-        },
-        {
-            scale: 0.75,
-            scrollTrigger: {
-                scroller: "#boxes",
-                trigger: "#boxes",
-                start: "160% right",
-                end: "+=60%",
-                horizontal: true,
-                scrub: true,
-                snap: {
-                    snapTo: 1,
-                    duration: { min: 0.2, max: 0.5 },
-                    delay: 0
-                }
-            }
-        }
-    );
+const productBoxes = document.querySelectorAll('.product-box');
+const prevButton = document.getElementById('prev-btn');
+const nextButton = document.getElementById('next-btn');
+let currentBox = 1; // Initial box at the center
 
-    gsap.fromTo(
-        "#box-2",
-        {
-            scale: 0.9
-        },
-        {
-            scale: 0.75,
-            scrollTrigger: {
-                scroller: "#boxes",
-                trigger: "#boxes",
-                start: "0% left",
-                end: "+=60%",
-                horizontal: true,
-                scrub: true
-            }
-        }
-    );
-    gsap.fromTo(
-        "#box-3",
-        {
-            scale: 0.75
-        },
-        {
-            scale: 0.9,
-            scrollTrigger: {
-                scroller: "#boxes",
-                trigger: "#boxes",
-                start: "160% right",
-                end: "+=60%",
-                horizontal: true,
-                scrub: true
-            }
-        }
-    );
+function updateProductBoxStyles() {
+    productBoxes.forEach((box, index) => {
+        const distance = Math.abs(currentBox - index);
+        const scale = 1 - distance * 0.1;
+        const background = index === currentBox ? '#ffcfcf' : '#ccadbe';
+        const zIndex = index === currentBox ? 1 : 0;
+
+        box.style.transform = `scale(${scale})`;
+        box.style.background = background;
+        box.style.zIndex = zIndex;
+    });
+}
+
+function navigate(direction) {
+    if (direction === 'prev' && currentBox > 0) {
+        currentBox--;
+    } else if (direction === 'next' && currentBox < productBoxes.length - 1) {
+        currentBox++;
+    }
+
+    updateProductBoxStyles();
+}
+
+prevButton.addEventListener('click', () => navigate('prev'));
+nextButton.addEventListener('click', () => navigate('next'));
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        // Handle left arrow key press (previous item)
+        navigate("prev");
+    } else if (event.key === "ArrowRight") {
+        // Handle right arrow key press (next item)
+        navigate("next");
+    }
 });
+
+// Initial update
+updateProductBoxStyles();
